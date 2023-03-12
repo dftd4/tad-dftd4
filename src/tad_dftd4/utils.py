1f927e239e46cf42191d22f83b72c88676f8ab5a
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2023-03-12 21:48:17 +0100

    License

--- src/tad_dftd4/utils.py
+++ src/tad_dftd4/utils.py
@@ -1,17 +1,20 @@
 # This file is part of tad-dftd4.
-# SPDX-Identifier: Apache-2.0
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# SPDX-Identifier: LGPL-3.0-or-later
+# Copyright (C) 2022 Marvin Friede
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# tad-dftd4 is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# tad-dftd4 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with tad-dftd4. If not, see <https://www.gnu.org/licenses/>.
 """
 Miscellaneous functions
 =======================
