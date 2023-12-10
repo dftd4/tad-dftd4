Fix NaNs and add gradient and Hessian tests (#25)

src/tad_dftd4/__init__.py
@@ -69,7 +69,7 @@ Example
 >>> # total charge of both systems
 >>> charge = torch.tensor([0.0, 0.0])
 >>>
->>> # TPSS0-D4-ATM parameters
+>>> # TPSSh-D4-ATM parameters
 >>> param = {
 ...     "s6": positions.new_tensor(1.0),
 ...     "s8": positions.new_tensor(1.85897750),

src/tad_dftd4/charges.py
@@ -310,49 +310,57 @@ def solve(
         )
 
     eps = torch.tensor(torch.finfo(positions.dtype).eps, **dd)
+    zero = torch.tensor(0.0, **dd)
+    stop = torch.sqrt(torch.tensor(2.0 / math.pi, **dd))  # sqrt(2/pi)
 
     real = real_atoms(numbers)
-    mask = real_pairs(numbers, diagonal=True)
+    mask = real_pairs(numbers, diagonal=False)
 
     distances = torch.where(mask, cdist(positions, positions, p=2), eps)
     diagonal = mask.new_zeros(mask.shape)
     diagonal.diagonal(dim1=-2, dim2=-1).fill_(True)
 
-    rhs = torch.concat(
-        (
-            -model.chi[numbers] + torch.sqrt(cn) * model.kcn[numbers],
-            total_charge.unsqueeze(-1),
-        ),
-        dim=-1,
+    cn_sqrt = torch.sqrt(torch.clamp(cn, min=eps))
+    cc = torch.where(
+        real,
+        -model.chi[numbers] + cn_sqrt * model.kcn[numbers],
+        zero,
     )
+    rhs = torch.concat((cc, total_charge.unsqueeze(-1)), dim=-1)
 
+    # radii
     rad = model.rad[numbers]
-    gamma = 1.0 / torch.sqrt(rad.unsqueeze(-1) ** 2 + rad.unsqueeze(-2) ** 2)
+    rads = torch.clamp(rad.unsqueeze(-1) ** 2 + rad.unsqueeze(-2) ** 2, min=eps)
+    gamma = torch.where(mask, 1.0 / torch.sqrt(rads), zero)
+
+    # hardness
     eta = torch.where(
         real,
-        model.eta[numbers] + torch.sqrt(torch.tensor(2.0 / math.pi)) / rad,
+        model.eta[numbers] + stop / rad,
         torch.tensor(1.0, **dd),
     )
+
     coulomb = torch.where(
         diagonal,
         eta.unsqueeze(-1),
         torch.where(
             mask,
             torch.erf(distances * gamma) / distances,
-            torch.tensor(0.0, **dd),
+            zero,
         ),
     )
+
     constraint = torch.where(
         real,
         torch.ones(numbers.shape, **dd),
         torch.zeros(numbers.shape, **dd),
     )
-    zero = torch.zeros(numbers.shape[:-1], **dd)
+    zeros = torch.zeros(numbers.shape[:-1], **dd)
 
     matrix = torch.concat(
         (
             torch.concat((coulomb, constraint.unsqueeze(-1)), dim=-1),
-            torch.concat((constraint, zero.unsqueeze(-1)), dim=-1).unsqueeze(-2),
+            torch.concat((constraint, zeros.unsqueeze(-1)), dim=-1).unsqueeze(-2),
         ),
         dim=-2,
     )

src/tad_dftd4/model.py
@@ -159,15 +159,13 @@ class D4Model(TensorLike):
         Tensor
             Weights for the atomic reference systems.
         """
-        dd: DD = {"device": self.device, "dtype": self.dtype}
-
         if cn is None:
-            cn = torch.zeros(self.numbers.shape, **dd)
+            cn = torch.zeros(self.numbers.shape, **self.dd)
         if q is None:
-            q = torch.zeros(self.numbers.shape, **dd)
+            q = torch.zeros(self.numbers.shape, **self.dd)
 
         refc = params.refc.to(self.device)[self.numbers]
-        refq = params.refq.to(**dd)[self.numbers]
+        refq = params.refq.to(**self.dd)[self.numbers]
         mask = refc > 0
 
         # Due to the exponentiation, `norm` and `expw` may become very small
@@ -191,24 +189,27 @@ class D4Model(TensorLike):
         # weights `igw`, we have to use masks and explicitly implement the
         # formulas for exponentiation. Luckily, `igw` only takes on the values
         # 1 and 3.)
+        def refc_pow(n: int) -> Tensor:
+            return sum(
+                (torch.pow(tmp, i * self.wf) for i in range(1, n + 1)),
+                torch.tensor(0.0, device=tmp.device),
+            )
+
+        refc_pow_1 = torch.where(refc == 1, refc_pow(1), tmp)
+        refc_pow_final = torch.where(refc == 3, refc_pow(3), refc_pow_1)
+
         expw = torch.where(
             mask,
-            torch.where(
-                refc == 3,
-                torch.pow(tmp, self.wf)
-                + torch.pow(tmp, 2 * self.wf)
-                + torch.pow(tmp, 3 * self.wf),
-                torch.where(
-                    refc == 1,
-                    torch.pow(tmp, self.wf),
-                    tmp,
-                ),
-            ),
+            refc_pow_final,
             torch.tensor(0.0, device=self.device, dtype=torch.double),  # double!
         )
 
         # normalize weights
-        norm = torch.sum(expw, dim=-1, keepdim=True)
+        norm = torch.where(
+            mask,
+            torch.sum(expw, dim=-1, keepdim=True),
+            torch.tensor(1e-300, device=self.device, dtype=torch.double),  # double!)
+        )
         gw_temp = (expw / norm).type(self.dtype)  # back to real dtype
 
         # maximum reference CN for each atom
@@ -221,22 +222,22 @@ class D4Model(TensorLike):
             exceptional,
             torch.where(
                 refcn == maxcn,
-                torch.tensor(1.0, **dd),
-                torch.tensor(0.0, **dd),
+                torch.tensor(1.0, **self.dd),
+                torch.tensor(0.0, **self.dd),
             ),
             gw_temp,
         )
 
         # unsqueeze for reference dimension
         zeff = data.zeff.to(self.device)[self.numbers].unsqueeze(-1)
-        gam = data.gam.to(**dd)[self.numbers].unsqueeze(-1) * self.gc
+        gam = data.gam.to(**self.dd)[self.numbers].unsqueeze(-1) * self.gc
         q = q.unsqueeze(-1)
 
         # charge scaling
         zeta = torch.where(
             mask,
             self._zeta(gam, refq + zeff, q + zeff),
-            torch.tensor(0.0, **dd),
+            torch.tensor(0.0, **self.dd),
         )
 
         return zeta * gw
@@ -288,10 +289,14 @@ class D4Model(TensorLike):
         Tensor
             Scaled charges.
         """
+        eps = torch.tensor(torch.finfo(self.dtype).eps, **self.dd)
+        ga = torch.tensor(self.ga, **self.dd)
+        scale = torch.exp(gam * (1.0 - qref / (qmod - eps)))
+
         return torch.where(
             qmod > 0.0,
-            torch.exp(self.ga * (1.0 - torch.exp(gam * (1.0 - qref / qmod)))),
-            torch.exp(torch.tensor(self.ga, device=self.device, dtype=self.dtype)),
+            torch.exp(ga * (1.0 - scale)),
+            torch.exp(ga),
         )
 
     def _set_refalpha_eeq(self) -> Tensor:
@@ -303,21 +308,21 @@ class D4Model(TensorLike):
         Tensor
             Reference polarizibilities for unique species (not all atoms).
         """
-        zero = torch.tensor(0.0, device=self.device, dtype=self.dtype)
+        zero = torch.tensor(0.0, **self.dd)
 
         numbers = self.unique
-        refsys = params.refsys[numbers].to(self.device)
-        refsq = params.refsq[numbers].type(self.dtype).to(self.device)
-        refascale = params.refascale[numbers].type(self.dtype).to(self.device)
-        refalpha = params.refalpha[numbers].type(self.dtype).to(self.device)
-        refscount = params.refscount[numbers].type(self.dtype).to(self.device)
-        secscale = params.secscale.type(self.dtype).to(self.device)
-        secalpha = params.secalpha.type(self.dtype).to(self.device)
+        refsys = params.refsys.to(self.device)[numbers]
+        refsq = params.refsq.to(**self.dd)[numbers]
+        refascale = params.refascale.to(**self.dd)[numbers]
+        refalpha = params.refalpha.to(**self.dd)[numbers]
+        refscount = params.refscount.to(**self.dd)[numbers]
+        secscale = params.secscale.to(**self.dd)
+        secalpha = params.secalpha.to(**self.dd)
 
         mask = refsys > 0
 
-        zeff = data.zeff[refsys].to(self.device)
-        gam = data.gam[refsys].type(self.dtype).to(self.device) * self.gc
+        zeff = data.zeff.to(self.device)[refsys]
+        gam = data.gam[refsys].to(**self.dd) * self.gc
 
         aiw = secscale[refsys] * secalpha[refsys]
 

src/tad_dftd4/ncoord/eeq.py
@@ -28,7 +28,7 @@ import torch
 from .. import defaults
 from .._typing import Any, CountingFunction, Tensor
 from ..data import cov_rad_d3
-from ..utils import real_pairs
+from ..utils import cdist, real_pairs
 from .count import erf_count
 
 __all__ = ["coordination_number_eeq"]
@@ -80,8 +80,9 @@ def coordination_number_eeq(
         cutoff = torch.tensor(defaults.D4_CN_EEQ_CUTOFF, **dd)
 
     if rcov is None:
-        rcov = cov_rad_d3[numbers]
-    rcov = rcov.type(positions.dtype).to(positions.device)
+        rcov = cov_rad_d3.to(**dd)[numbers]
+    else:
+        rcov = rcov.to(**dd)
 
     if numbers.shape != rcov.shape:
         raise ValueError(
@@ -94,19 +95,12 @@ def coordination_number_eeq(
             f"with atomic numbers ({numbers.shape})."
         )
 
-    mask = real_pairs(numbers)
-    distances = torch.where(
-        mask,
-        torch.cdist(
-            positions,
-            positions,
-            p=2,
-            compute_mode="use_mm_for_euclid_dist",
-        ),
-        torch.tensor(torch.finfo(positions.dtype).eps, **dd),
-    )
+    eps = torch.tensor(torch.finfo(positions.dtype).eps, **dd)
 
-    rc = rcov.unsqueeze(-2) + rcov.unsqueeze(-1)
+    mask = real_pairs(numbers)
+    distances = torch.where(mask, cdist(positions, positions, p=2), eps)
+
+    rc = rcov.unsqueeze(-2) + rcov.unsqueeze(-1) + eps
     cf = torch.where(
         mask * (distances <= cutoff),
         counting_function(distances, rc, **kwargs),

tests/test_disp/test_full.py
@@ -58,7 +58,7 @@ def single(name: str, dtype: torch.dtype) -> None:
     charge = torch.tensor(0.0, **dd)
     ref = sample["disp"].to(**dd)
 
-    # TPSS0-D4-ATM parameters
+    # TPSSh-D4-ATM parameters
     param = {
         "s6": torch.tensor(1.00000000, **dd),
         "s8": torch.tensor(1.85897750, **dd),
@@ -132,7 +132,7 @@ def batch(name1: str, name2: str, dtype: torch.dtype) -> None:
         ]
     )
 
-    # TPSS0-D4-ATM parameters
+    # TPSSh-D4-ATM parameters
     param = {
         "s6": torch.tensor(1.00000000, **dd),
         "s8": torch.tensor(1.85897750, **dd),